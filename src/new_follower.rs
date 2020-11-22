24a4b3716749b91c21f7bb668d01490f1f0055cd
Mathias Wøbbe <mathias@kandelborg.dk>
2020-11-22 17:25:22 +0100

    only show images on linux

--- src/new_follower.rs
+++ src/new_follower.rs
@@ -2,34 +2,42 @@
 use serde::{Deserialize, Serialize};
 
 use notify_rust::Hint;
-#[cfg(all(feature = "images", unix, not(target_os = "macos")))]
+#[cfg(all(
+    feature = "images",
+    unix,
+    not(target_os = "macos"),
+    not(target_os = "windows")
+))]
 use notify_rust::Image;
 use notify_rust::Notification;
 
 use std::process::Command;
 
-use crate::common_structs::{Res, MetaMsg};
+use crate::common_structs::{MetaMsg, Res};
 
 #[derive(Deserialize, Serialize)]
 struct NewFollower {
     display_name: String,
     username: String,
-    user_id: String
+    user_id: String,
 }
 
 pub fn new_follower(res_msg: &Res) {
-    let new_follower: NewFollower = serde_json::from_str(res_msg.data.message.as_str()).expect("No json");
+    let new_follower: NewFollower =
+        serde_json::from_str(res_msg.data.message.as_str()).expect("Could not deserialize Twitch new follower");
 
     println!("{}", serde_json::to_string(&new_follower).expect(""));
 
-// Res {
-//    event: "MESSAGE",
-//    data: MetaMsg {
-//        topic: "following.268365847",
-//        message: "{\"display_name\":\"mathiaskandelborg\",\"username\":\"mathiaskandelborg\",\"u
-//ser_id\":\"589623537\"}",
-//    },
-//}
+    #[cfg(any(macos, windows))]
+    Notification::new()
+        .summary(format!("Tron awaits you, {}!", &new_follower.display_name).as_str())
+        .body("YOU ARE AMAZING! 🥰")
+        .unwrap()
+        .show()
+        .unwrap();
+
+
+    #[cfg(linux)]
     Notification::new()
         .summary(format!("Tron awaits you, {}!", &new_follower.display_name).as_str())
         .body("YOU ARE AMAZING! 🥰")
